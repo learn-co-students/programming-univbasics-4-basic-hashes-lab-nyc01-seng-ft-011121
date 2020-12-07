32c448cc965ce1202de5db6de0cc5e6f023355b8
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-07 21:39:43 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,16 @@
 def new_hash
-  # return an empty hash
+  nhash = {} 
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  avatars = {air: "aang", water: "korra", fire: "roku", earth: "kyoshi"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  phash = {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  ihash = {:id => number}
 end
